970130277861bda590564b2143663acf7518693b
ebinipe <ebinipe@gmail.com>
2019-06-28 09:47:17 +0530

    Updates

---
+++ precache-manifest.356b9e8f66864cdfbdfec2dc97f5dc7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8843faf50d6ddc49a2cffdde816a1dc7",
+    "url": "/Deploy-React/index.html"
+  },
+  {
+    "revision": "82f4c175d639909a1944",
+    "url": "/Deploy-React/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Deploy-React/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "82f4c175d639909a1944",
+    "url": "/Deploy-React/static/js/main.65e607b2.chunk.js"
+  },
+  {
+    "revision": "2633d02d2ba3af5fa4ca",
+    "url": "/Deploy-React/static/js/runtime~main.14e359d7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Deploy-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
