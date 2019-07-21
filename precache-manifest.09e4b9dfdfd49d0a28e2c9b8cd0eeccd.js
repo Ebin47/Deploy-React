8fa1fb3bfccf4a628f3fd7967d23d3191bced937
ebinipe <ebinipe@gmail.com>
2019-07-21 23:52:35 +0530

    Updates

--- precache-manifest.09e4b9dfdfd49d0a28e2c9b8cd0eeccd.js
+++ precache-manifest.09e4b9dfdfd49d0a28e2c9b8cd0eeccd.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5de20b552d2446594d0994c61779d1b8",
+    "revision": "257daa87afb78d4c87e0351b6bdeb8c3",
     "url": "/Deploy-React/index.html"
   },
   {
-    "revision": "19d4118f04b2589bc3fa",
-    "url": "/Deploy-React/static/css/main.8814e3f4.chunk.css"
+    "revision": "078f2723990544e99062",
+    "url": "/Deploy-React/static/css/main.38cb6fb4.chunk.css"
   },
   {
     "revision": "681b51057d74cfa8975d",
     "url": "/Deploy-React/static/js/2.65aa1cca.chunk.js"
   },
   {
-    "revision": "19d4118f04b2589bc3fa",
-    "url": "/Deploy-React/static/js/main.a10bad5e.chunk.js"
+    "revision": "078f2723990544e99062",
+    "url": "/Deploy-React/static/js/main.0c6922bc.chunk.js"
   },
   {
     "revision": "2633d02d2ba3af5fa4ca",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Deploy-React/precache-manifest.cacd30468ef44f47e30e6410964c729d.js"
+  "/Deploy-React/precache-manifest.09e4b9dfdfd49d0a28e2c9b8cd0eeccd.js"
 );
 
 self.addEventListener('message', (event) => {
